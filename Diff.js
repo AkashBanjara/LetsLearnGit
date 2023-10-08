2cae67ae3dbe2a2d65ecc874845a980f6b6e5451
Aakash banjara <Banjaraaakash5@gmail.com>
2023-10-08 06:07:41 +0530

    Diff and  mod sum.js commited

---
+++ Diff.js
@@ -0,0 +1,7 @@
+function Diff(){
+    let a=245;
+    let b=124;
+    let sub = a-b;
+    console.log(sub);
+}
+Diff()
--- sum.js
+++ sum.js
@@ -2,6 +2,6 @@ const sum=()=>{
     let a=34;
     let b=23;
     let total = a+b;
-    console.log(total);
+    console.log('the sum of two number is' + total);
 }
 sum()
